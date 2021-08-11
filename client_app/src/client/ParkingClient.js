1719010311270ac92e95fe91e4e3833aa9cb7661
Gustavo Segura <yo@gustavosegura.com>
2021-08-10 20:58:45 -0600

    Parking API client

--- client_app/src/client/ParkingClient.js
+++ client_app/src/client/ParkingClient.js
@@ -10,4 +10,25 @@ export class ParkingClient {
         const parsedResponse = await rawResponse.json();
         return parsedResponse;
     }
+
+    async createParkingReservation(carId, ownerName, contact) {
+        const rawResponse = await fetch("http://127.0.0.1:5000/reservations", {
+            method:"POST",
+            body:{ "carId":carId, "OwnerName":ownerName, "contact":contact}
+        });
+        const parsedResponse = await rawResponse.json();
+        return parsedResponse;
+    }
+
+    async createParkingSpace() {
+        const rawResponse = await fetch("http://127.0.0.1:5000/spaces", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({})
+        });
+        const parsedResponse = await rawResponse.json();
+        return parsedResponse;
+    }
+
+
 }
