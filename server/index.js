c801bb4ed3f684b866f9c602f953968637c3da35
Gustavo Segura <yo@gustavosegura.com>
2021-08-18 15:10:27 -0600

    query spaces and reservations

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const https = require('https');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
-const app = express();             
+const app = express();
 const port = 5000;
 app.use(express.json())
 app.use(cors())
@@ -13,51 +13,72 @@ console.log(__dirname)
 https.createServer({
     key: fs.readFileSync(path.join(__dirname, '../certificados/key.pem')),
     cert: fs.readFileSync(path.join(__dirname, '../certificados/cert.pem'))
-},app).listen(port, function(){
-	console.log('Servidor https correindo en el puerto 5000');
+}, app).listen(port, function () {
+    console.log('Servidor https correindo en el puerto 5000');
 });
 
 /**
  * Get method to the spaces 
  */
-app.get('/spaces', function(req, res){
+app.get('/spaces', function (req, res) {
     let content = JSON.parse(fs.readFileSync('./DB/spaces.json', 'utf8'));
     console.log(req.query);
     console.log(content.spaces);
     console.log(req.query)
-    res.status(200).send(content.spaces);
+    const filters = req.query;
+    const filteredSpaces = content.spaces.filter(space => {
+        let isValid = true;
+        for (key in filters) {
+            console.log(key, space[key], filters[key]);
+            isValid = isValid && space[key] == filters[key];
+        }
+        return isValid;
+    });
+    res.status(200).send(filteredSpaces);
 })
 
 /**
  * Get method with a param of the id in the spaces data
  */
-app.get('/spaces/:id', function(req, res){
+app.get('/spaces/:id', function (req, res) {
     let content = JSON.parse(fs.readFileSync('./DB/spaces.json', 'utf8'));
     console.log(req.params)
     let id = Number(req.params.id)
     const find = content.spaces.find(ele => ele.id === id);
-    if(find){
+    if (find) {
         res.status(200).send(find);
     }
-    else{
-        res.status(404).json({"method": "GET","param": id,"message": `space with the id ${id} is not found`});
+    else {
+        res.status(404).json({ "method": "GET", "param": id, "message": `space with the id ${id} is not found` });
     }
 })
 
 /**
  * Get the reservations data
  */
-app.get('/reservations', function(req, res){
+app.get('/reservations', function (req, res) {
     let content = JSON.parse(fs.readFileSync('./DB/spaces.json', 'utf8'));
     console.log(req.query)
     console.log(content.reservation);
-    res.status(200).send(content.reservation);
+
+    const filters = req.query;
+    const filteredReservation = content.reservation.filter(reservation => {
+        let isValid = true;
+        for (key in filters) {
+            console.log(key, reservation[key], filters[key]);
+            isValid = isValid && reservation[key] == filters[key];
+        }
+        return isValid;
+    });
+    res.status(200).send(filteredReservation);
+
+    // res.status(200).send(content.reservation);
 })
 
 /**
  * Create a space
  */
-app.post('/spaces', function(req, res){
+app.post('/spaces', function (req, res) {
     let content = JSON.parse(fs.readFileSync('./DB/spaces.json', 'utf8'));
     let body = req.body;
     body['id'] = Date.now();
@@ -65,24 +86,25 @@ app.post('/spaces', function(req, res){
     console.log(body);
     content.spaces.push(body);
     fs.writeFileSync('./DB/spaces.json', JSON.stringify(content));
-    res.status(200).json({"method": "POST","message": "The data is completely save","body":body});    
+    res.status(200).json({ "method": "POST", "message": "The data is completely save", "body": body });
 })
 
 /**
  * Method for create a new reservation
  */
-app.post('/reservations', function(req, res){
+app.post('/reservations', function (req, res) {
     let content = JSON.parse(fs.readFileSync('./DB/spaces.json', 'utf8'))
     let body = req.body;
     body['id'] = Date.now();
     body['time'] = Date.now();
     let index = content.spaces.findIndex(elem => elem.state === 'free');
-    if(index === -1){
+    if (index === -1) {
         res.status(404).json({
             "method": 'POST',
             "message": "There is not space for create a reservation",
-            "body": body})
-    }else{
+            "body": body
+        })
+    } else {
         body.space = content.spaces[index].id;
         content.spaces[index].state = "in use";
         content.reservation.push(body);
@@ -90,64 +112,69 @@ app.post('/reservations', function(req, res){
         res.status(200).json({
             "method": 'POST',
             "message": "The data is completely save",
-            "body":body});
+            "body": body
+        });
     }
 })
 
 /**
  * Method for update data in a space id
  */
-app.put('/spaces/:id', function(req, res){
+app.put('/spaces/:id', function (req, res) {
     let content = JSON.parse(fs.readFileSync('./DB/spaces.json', 'utf8'));
     let id = Number(req.params.id)
     const found = content.spaces.find(ele => ele.id === id);
-    if(found){
+    if (found) {
         found.state = req.body['state'];
         fs.writeFileSync('./DB/spaces.json', JSON.stringify(content));
         res.status(200).json({
             "method": 'PUT',
             "message": "The data is completely save",
-            "body": found});
-    }else{
+            "body": found
+        });
+    } else {
         res.status(404).json({
             "method": 'PUT',
             "message": `There is not space with the id ${id}`,
-            "param": id});
+            "param": id
+        });
     }
 })
 
 /**
  * Method to delete the an space
  */
-app.delete('/spaces/:id', function(req, res){
+app.delete('/spaces/:id', function (req, res) {
     let content = JSON.parse(fs.readFileSync('./DB/spaces.json', 'utf8'));
     let id = Number(req.params.id)
     const found = content.spaces.find(ele => ele.id === id);
     console.log(id, found);
-    if(found){
+    if (found) {
         let index = content.spaces.findIndex(ele => ele.id === id);
         content.spaces.splice(index, 1);
         fs.writeFileSync('./DB/spaces.json', JSON.stringify(content));
         res.status(200).json({
             method: 'DELETE',
             message: "The data is completely delete",
-            body: found});
-    }else{
+            body: found
+        });
+    } else {
         res.status(404).json({
             method: 'DELETE',
             message: `There is not space with the id ${id}`,
-            param: id});
+            param: id
+        });
     }
 })
 
 /**
  * Delete the reservation item
  */
-app.delete('/reservations/:id', function(req, res){
+app.delete('/reservations/:id', function (req, res) {
     let content = JSON.parse(fs.readFileSync('./DB/spaces.json', 'utf8'));
     let id = Number(req.params.id)
     const found = content.reservation.find(ele => ele.id === id);
-    if(found){
+    if (found) {
         let index = content.reservation.findIndex(ele => ele.id === id);
         let spaceChange = content.spaces.find(ele => ele.id === found.space);
         console.log(spaceChange)
@@ -157,16 +184,18 @@ app.delete('/reservations/:id', function(req, res){
         res.status(200).json({
             method: 'DELETE',
             message: "The data is completely delete",
-            body: found});
-    }else{
+            body: found
+        });
+    } else {
         res.status(404).json({
             method: 'DELETE',
             message: `There is not reservation with the id ${id}`,
-            param: id});
+            param: id
+        });
     }
 })
 
-app.use('*', (req, res)=>{
+app.use('*', (req, res) => {
     res.status(405)
     res.json('Route is not valid')
 })
